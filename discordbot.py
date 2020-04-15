eebe9e0ea272480aea765891f59532970e127c29
nalydayo <63695436+nalydayo@users.noreply.github.com>
2020-04-15 15:41:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,8 +14,8 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def neko(ctx):
-    await ctx.send('にゃーん')
+async def ping(ctx):
+    await ctx.send('pong')
 
     
 bot.run(token)
